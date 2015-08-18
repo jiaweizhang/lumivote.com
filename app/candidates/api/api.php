d79c915d9a80e8f34ff96de0feb9266037f0b25b
Jiawei Zhang <jiaweizhang95@hotmail.com>
2015-08-17 23:47:12 -0400

    Added backend including candidates and timeline APIs. Added backend authentication.

---
+++ app/candidates/api/api.php
@@ -0,0 +1,182 @@
+<?php
+$method = $_SERVER['REQUEST_METHOD'];
+$url = $_SERVER['REQUEST_URI'];
+$paths = array_filter( explode("/", $url));
+
+
+array_shift($paths);
+
+
+
+// set up the connection variables
+$db_name  = '';
+$hostname = '';
+$username = '';
+$password = '';
+
+// connect to the database
+$dbh = new PDO("mysql:host=$hostname;dbname=$db_name", $username, $password);
+
+if ($method == 'GET') {  
+ $queryString = $_SERVER['QUERY_STRING'];
+ parse_str($queryString, $outputQuery);
+ $party = "";
+ $ID = -1;
+ $party = $outputQuery['party'];
+ $ID = $outputQuery['ID'];
+ //echo $party;
+ //echo $ID;
+
+ if (strlen($party)!=0 ) {
+   if ($party == 'democratic') {
+    $sql = 'SELECT * FROM candidates WHERE party="Democratic"';
+  }
+  else if ($party == 'republican') {
+    $sql = 'SELECT * FROM candidates WHERE party="Republican"';
+  }
+  else if ($party == 'independent') {
+    $sql = 'SELECT * FROM candidates WHERE party<>"Democratic" AND party<>"Republican"';
+  }
+  $stmt = $dbh->prepare($sql);
+  $stmt->execute();
+
+
+  $result = $stmt->fetchAll( PDO::FETCH_ASSOC );
+
+  $json = json_encode( $result );
+
+  echo "{\"candidates\":" . $json . "}";
+  }
+  else if (strlen($ID)!=0) {
+  $sql = 'SELECT * FROM candidates WHERE ID=:ID';
+  $stmt = $dbh->prepare($sql);
+  $stmt->bindParam(':ID', $ID);
+  $stmt->execute();
+  $result = $stmt->fetchAll( PDO::FETCH_ASSOC );
+
+  $json = json_encode( $result );
+
+  echo "{\"candidates\":" . $json . "}";
+}  
+ 
+  else {
+
+  $sql = 'SELECT * FROM candidates';
+  $stmt = $dbh->prepare($sql);
+  $stmt->execute();
+
+
+  $result = $stmt->fetchAll( PDO::FETCH_ASSOC );
+
+  $json = json_encode( $result );
+
+  echo "{\"candidates\":" . $json . "}";
+}
+
+
+}
+else if ($method == 'POST') {
+  //var_dump($params);
+  $params = json_decode(file_get_contents('php://input'),true);
+  $fName = $params['fName'];
+  $mName = $params['mName'];
+  $lName = $params['lName'];
+  $party = $params['party'];
+  $occupation = $params['occupation'];
+  $birthdate = $params['birthdate'];
+  $spouseFName = $params['spouseFName'];
+  $spouseMName = $params['spouseMName'];
+  $spouseLName = $params['spouseLName'];
+  $bio = $params['bio'];
+  $twitter = $params['twitter'];
+  $url = $params['url'];
+  $sql = 'INSERT INTO candidates (fName, mName, lName, party, occupation, birthdate, spouseFName, spouseMName, spouseLName, bio, twitter, url) VALUES (:fName, :mName, :lName, :party, :occupation, :birthdate, :spouseFName, :spouseMName, :spouseLName, :bio, :twitter, :url)';
+
+  $stmt = $dbh->prepare($sql);
+
+  $stmt->bindParam(':fName', $fName);
+  $stmt->bindParam(':mName', $mName);
+  $stmt->bindParam(':lName', $lName);
+  $stmt->bindParam(':party', $party);
+  $stmt->bindParam(':occupation', $occupation);
+  $stmt->bindParam(':birthdate', $birthdate);
+  $stmt->bindParam(':spouseFName', $spouseFName);
+  $stmt->bindParam(':spouseMName', $spouseMName);
+  $stmt->bindParam(':spouseLName', $spouseLName);
+  $stmt->bindParam(':bio', $bio);
+  $stmt->bindParam(':twitter', $twitter);
+  $stmt->bindParam(':url', $url);
+
+  $stmt->execute();
+  echo "POST executed.";
+
+
+}
+else if ($method == 'DELETE') {
+  $queryString = $_SERVER['QUERY_STRING'];
+  parse_str($queryString, $outputQuery);
+  var_dump ($outputQuery);
+  $ID = $outputQuery['ID'];
+  $sql = 'DELETE FROM candidates WHERE ID=:ID';
+  $stmt = $dbh->prepare($sql);
+  $stmt->bindParam(':ID', $ID);
+  $stmt->execute();
+  echo "DELETE executed";
+}
+else if ($method == 'PUT') {
+  $queryString = $_SERVER['QUERY_STRING'];
+  parse_str($queryString, $outputQuery);
+
+  
+    //echo "put event";
+  $ID = $outputQuery['ID'];
+
+  $params = json_decode(file_get_contents('php://input'),true);
+  $fName = $params['fName'];
+  $mName = $params['mName'];
+  $lName = $params['lName'];
+  $party = $params['party'];
+  $occupation = $params['occupation'];
+  $birthdate = $params['birthdate'];
+  $spouseFName = $params['spouseFName'];
+  $spouseMName = $params['spouseMName'];
+  $spouseLName = $params['spouseLName'];
+  $bio = $params['bio'];
+  $twitter = $params['twitter'];
+  $url = $params['url'];
+
+  $sql = 'UPDATE candidates SET fName=:fName, mName=:mName, lName=:lName, party=:party, occupation=:occupation, birthdate=:birthdate, spouseFName=:spouseFName, spouseMName=:spouseMName, spouseLName=:spouseLName, bio=:bio, twitter=:twitter, url=:url WHERE ID=:ID';
+  $stmt = $dbh->prepare($sql);
+  $stmt->bindParam(":ID", $ID);
+  $stmt->bindParam(':fName', $fName);
+  $stmt->bindParam(':mName', $mName);
+  $stmt->bindParam(':lName', $lName);
+  $stmt->bindParam(':party', $party);
+  $stmt->bindParam(':occupation', $occupation);
+  $stmt->bindParam(':birthdate', $birthdate);
+  $stmt->bindParam(':spouseFName', $spouseFName);
+  $stmt->bindParam(':spouseMName', $spouseMName);
+  $stmt->bindParam(':spouseLName', $spouseLName);
+  $stmt->bindParam(':bio', $bio);
+  $stmt->bindParam(':twitter', $twitter);
+  $stmt->bindParam(':url', $url);
+
+  $stmt->execute();
+
+  echo "PUT executed";
+}
+
+
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
+function startsWith($haystack, $needle) {
+    // search backwards starting from haystack length characters from the end
+  return $needle === "" || strrpos($haystack, $needle, -strlen($haystack)) !== FALSE;
+}
+?>
---
+++ app/timeline/api/api.php
@@ -0,0 +1,122 @@
+<?php
+$method = $_SERVER['REQUEST_METHOD'];
+$url = $_SERVER['REQUEST_URI'];
+$paths = array_filter( explode("/", $url));
+
+//var_dump($paths);
+//echo "<br/>";
+array_shift($paths);
+
+
+
+// set up the connection variables
+$db_name  = '';
+$hostname = '';
+$username = '';
+$password = '';
+
+// connect to the database
+$dbh = new PDO("mysql:host=$hostname;dbname=$db_name", $username, $password);
+
+if ($method == 'GET') {  
+  $sql = 'SELECT * FROM timeline';
+
+  $stmt = $dbh->prepare($sql);
+  $stmt->execute();
+
+
+  $result = $stmt->fetchAll( PDO::FETCH_ASSOC );
+
+  $json = json_encode( $result );
+
+  echo "{\"timeline\":" . $json . "}";
+}
+else if ($method == 'POST') {
+  //var_dump($params);
+  $params = json_decode(file_get_contents('php://input'),true);
+  $name = $params['name'];
+  $date = $params['date'];
+  $time = $params['time'];
+  $party = $params['party'];
+  $city = $params['city'];
+  $state = $params['state'];
+  $type = $params['type'];
+  $description = $params['description'];
+  $sql = 'INSERT INTO timeline (name, date, time, party, city, state, type, description) VALUES (:name, :date, :time, :party, :city, :state, :type, :description)';
+
+  $stmt = $dbh->prepare($sql);
+
+  $stmt->bindParam(':name', $name);
+  $stmt->bindParam(':date', $date);
+  $stmt->bindParam(':time', $time);
+  $stmt->bindParam(':party', $party);
+  $stmt->bindParam(':city', $city);
+  $stmt->bindParam(':state', $state);
+  $stmt->bindParam(':type', $type);
+  $stmt->bindParam(':description', $description);
+
+  $stmt->execute();
+  echo "POST executed.";
+
+
+}
+else if ($method == 'DELETE') {
+  $queryString = $_SERVER['QUERY_STRING'];
+  parse_str($queryString, $outputQuery);
+  var_dump ($outputQuery);
+  $eventID = $outputQuery['eventID'];
+  $sql = 'DELETE FROM timeline WHERE eventID=:eventID';
+  $stmt = $dbh->prepare($sql);
+  $stmt->bindParam(':eventID', $eventID);
+  $stmt->execute();
+  echo "DELETE executed";
+}
+else if ($method == 'PUT') {
+  $queryString = $_SERVER['QUERY_STRING'];
+  parse_str($queryString, $outputQuery);
+
+  
+    //echo "put event";
+  $eventID = $outputQuery['eventID'];
+
+  $params = json_decode(file_get_contents('php://input'),true);
+  $name = $params['name'];
+  $date = $params['date'];
+  $time = $params['time'];
+  $party = $params['party'];
+  $city = $params['city'];
+  $state = $params['state'];
+  $type = $params['type'];
+  $description = $params['description'];
+
+  $sql = 'UPDATE timeline SET name=:name, date=:date, time=:time, party=:party, city=:city, state=:state, type=:type, description=:description WHERE eventID=:eventID';
+  $stmt = $dbh->prepare($sql);
+  $stmt->bindParam(":eventID", $eventID);
+  $stmt->bindParam(':name', $name);
+  $stmt->bindParam(':date', $date);
+  $stmt->bindParam(':time', $time);
+  $stmt->bindParam(':party', $party);
+  $stmt->bindParam(':city', $city);
+  $stmt->bindParam(':state', $state);
+  $stmt->bindParam(':type', $type);
+  $stmt->bindParam(':description', $description);
+
+  $stmt->execute();
+
+  echo "PUT executed";
+}
+
+
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
+function startsWith($haystack, $needle) {
+    // search backwards starting from haystack length characters from the end
+  return $needle === "" || strrpos($haystack, $needle, -strlen($haystack)) !== FALSE;
+}
+?>
