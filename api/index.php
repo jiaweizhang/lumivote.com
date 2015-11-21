a5715d8b532415605b085b50bf1face2bae3b41e
Jiawei Zhang <jiaweizhang95@gmail.com>
2015-11-21 00:58:47 -0500

    working user creation

--- api/index.php
+++ api/index.php
@@ -7,125 +7,6 @@ require 'Slim/Slim.php';
 
 $app = new \Slim\Slim();
 
-// Instantiate global user id from the db
-$user_id = NULL;
-
-
-/**
- * User Registration
- * url - /register
- * method - POST
- * params - name, email, password
- */
-$app->post('/register', function () use ($app) {
-    // check for required params
-    verifyRequiredParams(array('email', 'password'));
-
-    $response = array();
-
-    // reading post params
-    $email = $app->request->post('email');
-    $password = $app->request->post('password');
-
-    // validating email address
-    validateEmail($email);
-
-    $db = new DbHandler();
-    $res = $db->createUser($email, $password);
-
-    if ($res == USER_CREATED_SUCCESSFULLY) {
-        $response["error"] = false;
-        $response["message"] = "You are successfully registered";
-        echoRespnse(201, $response);
-    } else if ($res == USER_CREATE_FAILED) {
-        $response["error"] = true;
-        $response["message"] = "Oops! An error occurred while registereing";
-        echoRespnse(200, $response);
-    } else if ($res == USER_ALREADY_EXISTED) {
-        $response["error"] = true;
-        $response["message"] = "Sorry, this email already existed";
-        echoRespnse(200, $response);
-    }
-});
-
-/**
- * User Login
- * url - /login
- * method - POST
- * params - email, password
- */
-$app->post('/login', function () use ($app) {
-    // check for required params
-    verifyRequiredParams(array('email', 'password'));
-
-    // reading post params
-    $email = $app->request()->post('email');
-    $password = $app->request()->post('password');
-    $response = array();
-
-    $db = new DbHandler();
-    // check for correct email and password
-    if ($db->checkLogin($email, $password)) {
-        // get the user by email
-        $user = $db->getUserByEmail($email);
-        if ($user != NULL) {
-            $response["error"] = false;
-            $response['email'] = $user['email'];
-            $response['apiKey'] = $user['api_key'];
-            $response['createdAt'] = $user['created_at'];
-            $response['profileCreated'] = $user['profile_created'];
-        } else {
-            // unknown error occurred
-            $response['error'] = true;
-            $response['message'] = "An error occurred. Please try again";
-        }
-    } else {
-        // user credentials are wrong
-        $response['error'] = true;
-        $response['message'] = 'Login failed. Incorrect credentials';
-    }
-
-    echoRespnse(200, $response);
-});
-
-
-/**
- * Create user profile in db
- * method POST
- * params - name, unit, city, state, country
- * url - /profile
- */
-$app->post('/profile', 'authenticate', function () use ($app) {
-    verifyRequiredParams(array('name', 'unit', 'city', 'state', 'country'));
-
-    $response = array();
-    $name = $app->request->post('name');
-    $unit = $app->request->post('unit');
-    $city = $app->request->post('city');
-    $state = $app->request->post('state');
-    $country = $app->request->post('country');
-
-    global $user_id;
-    $db = new DbHandler();
-
-    // creating new task
-    $res = $db->createProfile($user_id, $name, $unit, $city, $state, $country);
-
-    if ($res == USERPROFILE_CREATED_SUCCESSFULLY) {
-        $response["error"] = false;
-        $response["message"] = "Profile successfully created.";
-        echoRespnse(201, $response);
-    } else if ($res == USERPROFILE_CREATE_FAILED) {
-        $response["error"] = true;
-        $response["message"] = "Oops! An error occurred while creating profile.";
-        echoRespnse(200, $response);
-    } else if ($res == USERPROFILE_ALREADY_EXISTED) {
-        $response["error"] = true;
-        $response["message"] = "Sorry, this profile already existed.";
-        echoRespnse(200, $response);
-    }
-});
-
 /**
  * User Creation
  */
@@ -135,25 +16,17 @@ $app->post('/user', function () use ($app) {
 
     $json = $app->request->getBody();
     $input = json_decode($json, true); // parse the JSON into an assoc. array
-    // do other stuff
-    //echoRespnse(200, $user);
-
-    var_dump($input['user']);
 
     $user = $input['user'];
-
-    echo $user['username'];
 
     $db = new DbHandler();
     $res = $db->createUser($user);
 
     if ($res == USER_CREATED_SUCCESSFULLY) {
-        $response["error"] = false;
-        $response["message"] = "You are successfully registered";
+        $response = array("error" => false,   "message" => "You are successfully registered");
         echoRespnse(201, $response);
     } else if ($res == USER_CREATE_FAILED) {
-        $response["error"] = true;
-        $response["message"] = "Oops! An error occurred while registering";
+        $response = array("error" => true,   "message" => "Oops! An error occurred while registering");
         echoRespnse(200, $response);
     } else if ($res == USER_ALREADY_EXISTED) {
         $response["error"] = true;
@@ -272,49 +145,11 @@ $app->get('/candidates', function () use ($app) {
     echoRespnse(200, $response);
 });
 
-
-/**
- * Adding Middle Layer to authenticate every request
- * Checking if the request has valid api key in the 'Authorization' header
- */
-function authenticate() {
-    $app = \Slim\Slim::getInstance();
-    // Getting request headers
-    $headers = $app->request->headers;
-    $response = array();
-    // Verifying Authorization Header
-    if (isset($headers['Authorization'])) {
-        $db = new DbHandler();
-
-        // get the api key
-        $api_key = $headers['Authorization'];
-        // validating api key
-        if (!$db->isValidApiKey($api_key)) {
-            // api key is not present in users table
-            $response["error"] = true;
-            $response["message"] = "Access Denied. Invalid Api key";
-            echoRespnse(401, $response);
-            $app->stop();
-        } else {
-            global $user_id;
-            // get user primary key id
-            $user_id = $db->getUserId($api_key)['id'];
-        }
-    } else {
-        // api key is missing in header
-        $response["error"] = true;
-        $response["message"] = "Api key is misssing";
-        echoRespnse(400, $response);
-        $app->stop();
-    }
-}
-
-;
-
 /**
  * Verifying required params posted or not
  */
-function verifyRequiredParams($required_fields) {
+function verifyRequiredParams($required_fields)
+{
     $error = false;
     $error_fields = "";
     $request_params = array();
@@ -346,7 +181,8 @@ function verifyRequiredParams($required_fields) {
 /**
  * Validating email address
  */
-function validateEmail($email) {
+function validateEmail($email)
+{
     $app = \Slim\Slim::getInstance();
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
         $response["error"] = true;
@@ -364,7 +200,8 @@ function validateEmail($email) {
  * @param Int $response
  *            Json response
  */
-function echoRespnse($status_code, $response) {
+function echoRespnse($status_code, $response)
+{
     $app = \Slim\Slim::getInstance();
     // Http response code
     $app->status($status_code);
