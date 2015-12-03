7c516c6deaa4a84cd5f75b57f8e57409e5d41e9c
Alex Dao <azd2@duke.edu>
2015-12-02 21:19:03 -0500

    Removed the "contact us" page

--- js/script.js
+++ js/script.js
@@ -37,25 +37,15 @@ myApp.config(function ($routeProvider) {
         .when('/about', {
             templateUrl: 'pages/about.html',
             controller: 'aboutController'
-        })
-
-        .when('/contact', {
-            templateUrl: 'pages/contact.html',
-            controller: 'contactController'
         });
 });
 
 myApp.controller('mainController', function ($scope) {
-    // create a message to display in our view
-    $scope.message = 'Everyone come and see how good I look!';
+
 });
 
 myApp.controller('aboutController', function ($scope) {
-    $scope.message = 'Look! I am an about page.';
-});
 
-myApp.controller('contactController', function ($scope) {
-    $scope.message = 'Contact us! JK. This is just a demo.';
 });
 
 myApp.controller('timelineController', function ($scope, $http) {
